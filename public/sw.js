3cc2b964d17d5d7ac4033ed4a2b7aa8fe5938eeb
Koushik <rskkoushik@gmail.com>
2023-04-21 13:41:22 +0530

    check pwa push notifications

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("push", function (e) {
+  console.log("Push Received...");
+  self.registration.showNotification("Test Notification title", {
+    body: "New Maintenance bill added to the apartments",
+    badge: "/apartment_icon_192.png",
+  });
+});
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import App from "./App";
 import "./index.css";
 import Providers from "./Providers";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register(
+    import.meta.env.MODE === "production" ? "/sw.js" : "/dev-sw.js?dev-sw",
+    { type: import.meta.env.MODE === "production" ? "classic" : "module" }
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Providers>

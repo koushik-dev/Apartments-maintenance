ccd8611d420f6e1c3a432e97ff00a2ef7adc2fc5
Koushik <rskkoushik@gmail.com>
2023-06-08 17:26:48 +0530

    cleanup

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { Layout } from "./Layout";
 export { Table } from "./Table";
-export { AmountInfo } from "./AmountInfo";
 export { Sidebar } from "./Sidebar";
 export { Modal as AddUserModal } from "./AddUser";
 export { Modal as AddExpenseModal } from "./AddExpense";
